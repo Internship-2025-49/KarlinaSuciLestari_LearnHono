7a09026dee43f065ddf54cb6acb95badb98abd96
karlinasuci <karlinasuci026@gmail.com>
2025-02-05 14:35:11 +0700

    feat: Insert Data into Database #1.

--- app/api/[...route]/routes/index.ts
+++ app/api/[...route]/routes/index.ts
@@ -2,13 +2,17 @@
 import { Hono } from 'hono';
 
 // import controller
-import { getPosts } from '../../../../controllers/PostController';  
+import { getPosts, createPost } from '../../../../controllers/PostController';  
 
 // inisialisasi router
 const router = new Hono();
 
 // routing posts index
 router.get('/', (c) => getPosts(c));  
+
+//routes posts create
+router.post('/', (c) => createPost(c));
+
 
 // Ekspor router untuk digunakan di aplikasi utama
 export const Routes = router;
