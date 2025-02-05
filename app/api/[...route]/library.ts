992b9f620fc19c8101eaabaa210f4abbb7b207cf
karlinasuci <karlinasuci026@gmail.com>
2025-02-05 10:27:06 +0700

    test: change the contents of the Jsonplaceholder with the data that has been created inside and delete the data that has been variable #2

--- app/api/[...route]/library.ts
+++ app/api/[...route]/library.ts
@@ -3,25 +3,26 @@ import { Hono } from "hono";
 const library = new Hono();
 
 
-const bookCategories = [
-  { id: 1, category: "Fiksi" },
-  { id: 2, category: "Non-Fiksi" },
-  { id: 3, category: "Sains" },
-  { id: 4, category: "Sejarah" },
-  { id: 5, category: "Biografi" },
-  { id: 6, category: "Teknologi" },
-];
+library.get("/post",async (c) =>{
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await response.json();
+
+    
+    return c.json({
+      ...data,
+    });
+  } catch (error) {
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
 
 
-library.get("/:id", async (c) => {
+library.get("/posts/:id", async (c) => {
   const id = Number(c.req.param("id")); 
 
   try {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    if (!response.ok) {
-      return c.json({ error: "Data not found" }, 404);
-    }
-
     const data = await response.json();
 
     
