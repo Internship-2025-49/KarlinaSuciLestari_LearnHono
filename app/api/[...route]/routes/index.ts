0050420dc3a58beb591b2aeaf82a95139b8256e5
karlinasuci <karlinasuci026@gmail.com>
2025-02-05 15:20:05 +0700

    feat: Displays Detailed Data By ID #3.

--- app/api/[...route]/routes/index.ts
+++ app/api/[...route]/routes/index.ts
@@ -2,7 +2,7 @@
 import { Hono } from 'hono';
 
 // import controller
-import { getPosts, createPost } from '../../../../controllers/PostController';  
+import { getPosts, createPost, getPostById } from '../../../../controllers/PostController';  
 
 // inisialisasi router
 const router = new Hono();
@@ -13,6 +13,8 @@ router.get('/', (c) => getPosts(c));
 //routes posts create
 router.post('/', (c) => createPost(c));
 
+//routes posts detail
+router.get('/:id', (c) => getPostById(c));
 
 // Ekspor router untuk digunakan di aplikasi utama
 export const Routes = router;
